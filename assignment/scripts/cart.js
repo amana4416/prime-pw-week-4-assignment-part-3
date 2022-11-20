f757c3a359e9921292ca8b7bd76615c415b24512
Aman Abdirazak <amanabdirizak@icloud.com>
2022-11-20 17:51:33 -0600

    first test done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,24 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// Create a global variable named `basket` and set it to an empty arra
+let basket = [];
+
+// Create a function called `addItem`
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+
+// Test function
+console.log('Add protein powder to basket:', addItem('protein powder'));
+console.log('Add yogurt to basket:', addItem('yogurt'));
+console.log('Add bagels to basket:', addItem('bagels'));
+
+// Create a function called `listItems`
+function listItems(array) {
+    for (let item of array) {
+        console.log(item);
+    }
+}
